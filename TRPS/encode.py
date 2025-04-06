a003147a001c4e70a7d778d27e1cddc487955717
Janetorn <me2000zver191614@gmail.com>
2025-04-06 19:03:40 +0300

    Update encode.py

--- TRPS/encode.py
+++ TRPS/encode.py
@@ -15,21 +15,20 @@ def encode(string, key=pwd):
     encoded_chars = []
     for i in range(len(string)):
         key_c = key[i % len(key)]
-        # ord() gives the respective ascii value
-        encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
+        # Зашифровка символов
+        encoded_c = chr((ord(string[i]) + ord(key_c)) % 256)  # исправить расчет
         encoded_chars.append(encoded_c)
     encoded_string = "".join(encoded_chars)
-    # Ensure bytes are passed to base64 encoding
+    # Убедитесь, что ваша строка закодирована в правильном формате
     return base64.urlsafe_b64encode(encoded_string.encode('utf-8')).decode('utf-8')
 
 def decode(string, key=pwd):
     decoded_chars = []
-    # utf-8 to avoid character mapping errors
-    # Decode from base64, then decode bytes to string
+    # Убедитесь, что вы декодируете корректно
     string = base64.urlsafe_b64decode(string.encode('utf-8')).decode('utf-8')
     for i in range(len(string)):
         key_c = key[i % len(key)]
-        encoded_c = chr(abs(ord(string[i]) - ord(key_c) % 256))
+        encoded_c = chr((ord(string[i]) - ord(key_c)) % 256)  # исправить расчет
         decoded_chars.append(encoded_c)
     decoded_string = "".join(decoded_chars)
     return decoded_string
