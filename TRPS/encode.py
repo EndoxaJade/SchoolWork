2e48d7836aa76ce3b69690c9b4ab373f91c3b0df
EndoxaJade <mangl.05.xxz@gmail.com>
2025-04-27 10:26:13 +0300

    Update encode.py
    Comment changes

--- TRPS/encode.py
+++ TRPS/encode.py
@@ -9,22 +9,20 @@ except IOError:
 
 pwd = "G!thuB"
 
-# A encoding similar to Vigenere cipher
+# An encoding similar to Vigenere cipher
 
 def encode(string, key=pwd):
     encoded_chars = []
     for i in range(len(string)):
         key_c = key[i % len(key)]
-        # Зашифровка символов
+        # Encoding symbols
         encoded_c = chr((ord(string[i]) + ord(key_c)) % 256)  # исправить расчет
         encoded_chars.append(encoded_c)
     encoded_string = "".join(encoded_chars)
-    # Убедитесь, что ваша строка закодирована в правильном формате
     return base64.urlsafe_b64encode(encoded_string.encode('utf-8')).decode('utf-8')
 
 def decode(string, key=pwd):
     decoded_chars = []
-    # Убедитесь, что вы декодируете корректно
     string = base64.urlsafe_b64decode(string.encode('utf-8')).decode('utf-8')
     for i in range(len(string)):
         key_c = key[i % len(key)]
